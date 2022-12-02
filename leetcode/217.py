200c39883c67061e61b637ded9b56cf9e53799b7
taylancode <88141669+taylancode@users.noreply.github.com>
2022-12-02 19:43:10 +0000

    217 complete

--- leetcode/217.py
+++ leetcode/217.py
@@ -1,7 +1,18 @@
 from codingchallenges.utils.decorators import Decorators as d
+from typing import List
 
-@d.timer
-def test ():
-    print(1)
+class Solution:
+    @d.timer
+    def containsDuplicate(self, nums: List[int]) -> bool:
 
-test()
+        container = set()
+
+        for num in nums:
+            if num not in container:
+                container.add(num)
+            else:
+                return True
+        return False
+
+if __name__ == '__main__':
+    assert Solution().containsDuplicate([1,1,1,3,3,4,3,2,4,2]) is True
